ec16391a1a000081d8896e8467d3e09ccfb7a598
CITI-US\vrathor <vrathor@citi-us.com>
2022-05-25 06:41:52 +0530

    new code

---
+++ src/test/java/com/vijayCode/MappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vijayCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
